ba79e13c47f2c8ac78da78181da5613ab83d4126
[capelito-tv-show] <capelito.tv.show@gmail.com>
2019-03-24 14:33:00 +0900

    add gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-97856171-1`,
       },
     },
     `gatsby-plugin-feed`,
